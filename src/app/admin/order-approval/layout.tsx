eefd1a0ca4ee96740b87bb03a47a0981950e1654
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-07-23 15:40:58 -0500

    Move links into order approval subnav

---
+++ src/app/admin/order-approval/layout.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { ReactNode } from "react";
+
+export default function Layout({ children }: { children: ReactNode }) {
+  return (
+    <>
+      <nav style={{ background: "none" }}>
+        <ul className="links-row">
+          <li>
+            <Link href="/admin/order-approval" className="normal-link">
+              Workflows
+            </Link>
+          </li>
+          <li>
+            <Link
+              href="/admin/order-approval/webstores"
+              className="normal-link"
+            >
+              Webstores
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      {children}
+    </>
+  );
+}
--- src/app/admin/order-approval/page.tsx
+++ src/app/admin/order-approval/page.tsx
@@ -1,14 +1,12 @@
 import { getWorkflowsWithIncludes } from "@/db/access/orderApproval";
 import GenericTable from "@/components/GenericTable";
 import { ResultsTable } from "./ResultsTable";
-import Link from "next/link";
 
 export default async function OrderApproval() {
   const workflows = await getWorkflowsWithIncludes();
 
   return (
     <>
-      <Link href="order-approval/webstores">View Webstores &gt;</Link>
       <h1>Workflows</h1>
       <p>
         A workflow is a series of steps that will be executed after an order is
