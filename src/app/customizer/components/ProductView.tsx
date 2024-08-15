e671c7437d23b8de0c6f9ade410457f9d87dc7bf
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-08-15 10:03:46 -0500

    Add konva

--- src/app/customizer/components/ProductView.tsx
+++ src/app/customizer/components/ProductView.tsx
@@ -3,16 +3,23 @@
 import styles from "@/styles/customizer/CustomProductDesigner.module.css";
 import { IMAGE_NOT_FOUND_URL } from "@/constants";
 import { useEditor } from "../EditorContext";
+import { Image, Layer, Stage } from "react-konva";
+import useImage from "use-image";
 
 export function ProductView() {
   const { selectedView } = useEditor();
+  const [image] = useImage(selectedView?.imageUrl || IMAGE_NOT_FOUND_URL);
 
   return (
-    <div className={styles["product-view-frame"]}>
-      <img
-        className={styles["contained-img"]}
-        src={selectedView?.imageUrl || IMAGE_NOT_FOUND_URL}
-      />
-    </div>
+    <Stage
+      width={650}
+      height={650}
+      style={{ position: "absolute", left: "300px", top: "25px" }}
+      scale={{ x: 1, y: 1 }}
+    >
+      <Layer>
+        <Image image={image} width={650} height={650} />
+      </Layer>
+    </Stage>
   );
 }
