ccea31b0609e2a58c0b49fdd518616d9ac123958
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-08-15 10:51:21 -0500

    Add abstraction for transformable objects

--- src/app/customizer/components/ProductView.tsx
+++ src/app/customizer/components/ProductView.tsx
@@ -10,6 +10,9 @@ import { Transformable } from "./productView/Transformable";
 export function ProductView() {
   const { selectedView } = useEditor();
   const [image] = useImage(selectedView?.imageUrl || IMAGE_NOT_FOUND_URL);
+  const [imageDog] = useImage(
+    "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"
+  );
 
   return (
     <Stage
@@ -20,7 +23,12 @@ export function ProductView() {
     >
       <Layer>
         <Image image={image} width={650} height={650} />
-        <Transformable />
+        <Transformable>
+          <Image image={imageDog} width={200} height={200} />
+        </Transformable>
+        <Transformable>
+          <Image image={imageDog} width={200} height={200} x={200} />
+        </Transformable>
       </Layer>
     </Stage>
   );
--- src/app/customizer/components/productView/Transformable.tsx
+++ src/app/customizer/components/productView/Transformable.tsx
@@ -1,38 +1,28 @@
 import Konva from "konva";
-import { useEffect, useRef } from "react";
-import { Image, Rect, Transformer } from "react-konva";
-import useImage from "use-image";
+import { ReactNode, useEffect, useRef } from "react";
+import { Group, Transformer } from "react-konva";
 
-export function Transformable() {
-  const [image] = useImage(
-    "https://hips.hearstapps.com/hmg-prod/images/dog-puppy-on-garden-royalty-free-image-1586966191.jpg?crop=0.752xw:1.00xh;0.175xw,0&resize=1200:*"
-  );
-  const imgRef = useRef<Konva.Image>(null);
-  const shapeRef = useRef<Konva.Rect>(null);
+type Props = {
+  children: ReactNode;
+};
+export function Transformable({ children }: Props) {
+  const mainRef = useRef<Konva.Group>(null);
   const transformerRef = useRef<Konva.Transformer>(null);
 
   useEffect(() => {
     const transformer = transformerRef.current;
-    const shape = shapeRef.current;
-    const img = imgRef.current;
-    if (!transformer || !img) return;
+    const main = mainRef.current;
+    if (!transformer || !main) return;
 
-    transformer.nodes([img]);
+    transformer.nodes([main]);
     transformer.getLayer()?.batchDraw();
   }, []);
 
   return (
     <>
-      {/* <Rect
-        ref={shapeRef}
-        x={100}
-        y={100}
-        width={100}
-        height={100}
-        fill={"red"}
-        draggable
-      /> */}
-      <Image image={image} width={300} height={300} ref={imgRef} draggable />
+      <Group ref={mainRef} draggable>
+        {children}
+      </Group>
       <Transformer ref={transformerRef} />
     </>
   );
