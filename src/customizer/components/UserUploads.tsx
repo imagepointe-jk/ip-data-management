ba9c5b84ab6352cdf2d7e7d3b3570ae362b8b67a
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2024-09-16 14:52:35 -0500

    Add styling and basic error message for image uploads

--- src/customizer/components/UserUploads.tsx
+++ src/customizer/components/UserUploads.tsx
@@ -1,8 +1,13 @@
 import { uploadMediaAction } from "@/actions/wordpress";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
+import styles from "@/styles/customizer/CustomProductDesigner/upload.module.css";
+import { LoadingIndicator } from "@/components/LoadingIndicator";
 
 export function UserUploads() {
+  const [status, setStatus] = useState("idle" as "idle" | "loading" | "error");
+
   async function onChoose(e: ChangeEvent<HTMLInputElement>) {
+    if (status === "loading") return;
     const files = e.target.files;
     if (!files) return;
     const file = files[0];
@@ -15,17 +20,32 @@ export function UserUploads() {
     const formData = new FormData();
     formData.append("file", withNewFilename);
 
+    setStatus("loading");
     try {
       const hostedUrl = await uploadMediaAction(formData, 8);
       console.log("uploaded at", hostedUrl);
+      setStatus("idle");
     } catch (error) {
+      setStatus("error");
       console.error(error);
     }
   }
 
   return (
-    <div>
-      <input type="file" onChange={onChoose} />
+    <div className={styles["main"]}>
+      <label
+        htmlFor="customizer-upload"
+        className={styles["upload-label-as-button"]}
+      >
+        {status !== "loading" && <>Upload</>}
+        {status === "loading" && (
+          <LoadingIndicator className={styles["spinner"]} />
+        )}
+        <input id="customizer-upload" type="file" onChange={onChoose} />
+      </label>
+      {status === "error" && (
+        <div style={{ color: "red" }}>There was an error.</div>
+      )}
     </div>
   );
 }
