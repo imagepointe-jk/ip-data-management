328e4f5cc70a99f1b64885b49d2f47a4c1af0bcc
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-01-31 09:00:14 -0600

    Remove temp route

--- src/app/api/test-route/route.ts
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-
-export function GET() {
-  const date = new Date();
-  return NextResponse.json({
-    message: `The current time is ${date.toLocaleString()}`,
-  });
-}
