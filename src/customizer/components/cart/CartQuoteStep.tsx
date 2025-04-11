c7f6db951ecbdda3ae5c525778a304012b56b904
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-04-11 14:46:35 -0500

    Improve contact form appearance

--- src/customizer/components/cart/CartQuoteStep.tsx
+++ src/customizer/components/cart/CartQuoteStep.tsx
@@ -9,48 +9,41 @@ type Props = {
 };
 export function CartQuoteStep({ showError, onClickBack, submitting }: Props) {
   return (
-    <>
-      <div>
-        <h2>Contact Information</h2>
-        <div className={styles["contact-form"]}>
-          <div>
-            <label htmlFor="first-name">First Name</label>
-            <input type="text" name="first-name" id="first-name" required />
-          </div>
-          <div>
-            <label htmlFor="last-name">Last Name</label>
-            <input type="text" name="last-name" id="last-name" required />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" required />
-          </div>
-          <div>
-            <label htmlFor="company">Union/Organization</label>
-            <input type="text" name="company" id="company" required />
-          </div>
-          <div>
-            <label htmlFor="local">Union Local</label>
-            <input type="text" name="local" id="local" />
-          </div>
-          <div>
-            <label htmlFor="comments">Comments/Questions</label>
-            <textarea
-              name="comments"
-              id="comments"
-              cols={50}
-              rows={6}
-            ></textarea>
-          </div>
+    <div className={styles["quote-step-main"]}>
+      <h2>Contact Information</h2>
+      <div className={styles["primary-fields"]}>
+        <div>
+          <label htmlFor="first-name">First Name</label>
+          <input type="text" name="first-name" id="first-name" required />
         </div>
-        {showError && (
-          <div className={styles["request-error"]}>
-            Error submitting request. Please try again later, or{" "}
-            <a href={CONTACT_US_URL}>contact us</a> for assistance.
-          </div>
-        )}
+        <div>
+          <label htmlFor="last-name">Last Name</label>
+          <input type="text" name="last-name" id="last-name" required />
+        </div>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="email" required />
+        </div>
+        <div>
+          <label htmlFor="company">Union/Organization</label>
+          <input type="text" name="company" id="company" required />
+        </div>
+        <div>
+          <label htmlFor="local">Union Local</label>
+          <input type="text" name="local" id="local" />
+        </div>
+        <div className={styles["comments-container"]}>
+          <label htmlFor="comments">Comments/Questions</label>
+          <textarea name="comments" id="comments" rows={6}></textarea>
+        </div>
       </div>
-      <div className={styles["step-buttons-container"]}>
+      {showError && (
+        <div className={styles["request-error"]}>
+          Error submitting request. Please try again later, or{" "}
+          <a href={CONTACT_US_URL}>contact us</a> for assistance.
+        </div>
+      )}
+      <div className={styles["quote-buttons-container"]}>
         <button className="button-minor" onClick={onClickBack}>
           Back
         </button>
@@ -62,6 +55,6 @@ export function CartQuoteStep({ showError, onClickBack, submitting }: Props) {
           spinnerClassName={styles["submit-spinner"]}
         />
       </div>
-    </>
+    </div>
   );
 }
