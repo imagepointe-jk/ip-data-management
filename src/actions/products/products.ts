7f47131ee46c106129afcc307bdcd9688425059d
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-04-14 13:11:00 -0500

    Add basic product spreadsheet parsing

--- src/actions/products/products.ts
+++ src/actions/products/products.ts
@@ -1,5 +1,24 @@
 "use server";
 
+import { AppError } from "@/error";
+import { validateASIProducts } from "@/types/validations/products";
+import { getSheetFromBuffer } from "@/utility/spreadsheet";
+import { BAD_REQUEST } from "@/utility/statusCodes";
+
 export async function uploadSyncData(formData: FormData) {
-  console.log("run");
+  const file = formData.get("file");
+  if (!(file instanceof File) || file.size === 0) {
+    throw new AppError({
+      type: "Client Request",
+      clientMessage: "Invalid or missing file.",
+      serverMessage: "Invalid or missing file.",
+      statusCode: BAD_REQUEST,
+    });
+  }
+
+  const arrayBuffer = await file.arrayBuffer();
+  const asiSheet = getSheetFromBuffer(Buffer.from(arrayBuffer), "ASI");
+  const parsed = validateASIProducts(asiSheet);
+
+  console.log(parsed[0]);
 }
---
+++ src/types/validations/products.ts
@@ -0,0 +1,90 @@
+import { getSheetCellValue } from "@/utility/spreadsheet";
+import { WorkSheet } from "xlsx";
+import { ASIProductImportData } from "../schema/products";
+
+const MAX_EMPTY_SKU_CELLS = 100; //if we find this many empty cells in a row in the SKU column, assume that we have found all product entries in the spreadsheet
+const ASI_SKU_COLUMN = 2;
+const ASI_PRICING_TABLE_START_COLUMN = 6;
+const ASI_PRICING_TABLE_END_COLUMN = 15;
+const ASI_PRICING_TABLE_ROW_OFFSET = 6; //the pricing table starts this many rows below the SKU row
+const ASI_VENDOR_NAME_ROW_OFFSET = 1;
+const ASI_VENDOR_SKU_ROW_OFFSET = 2;
+const ASI_DESCRIPTION_ROW_OFFSET = 4;
+const ASI_PRIMARY_DATA_COLUMN = 4;
+
+//the spreadsheet is structured in a very specific way and data for each product fills up multiple rows.
+//zod is not very helpful in this case, so use custom parsing logic instead.
+export function validateASIProducts(sheet: WorkSheet) {
+  const products: ASIProductImportData[] = [];
+  let emptySkuCells = 0; //tracks how many cells in the "SKU" column have been empty. resets to 0 when a value in this column is found.
+  for (let i = 1; i < Number.MAX_SAFE_INTEGER; i++) {
+    const sku = getSheetCellValue(ASI_SKU_COLUMN, i, sheet);
+    if (sku) {
+      emptySkuCells = 0;
+      try {
+        const data = pullLocalProductValues(i, sheet);
+        products.push(data);
+      } catch (error) {
+        console.error(`Error parsing product data for sku ${sku}: ${error}`);
+      }
+    } else {
+      emptySkuCells++;
+    }
+
+    if (i > 999999) {
+      console.error("Looped through too many rows!"); //this should never print
+      break;
+    }
+
+    if (emptySkuCells >= MAX_EMPTY_SKU_CELLS) break;
+  }
+
+  return products;
+}
+
+//called when a SKU has been found; pull values from the area near the SKU based on predetermined spreadsheet formatting/structure
+function pullLocalProductValues(
+  rowNumber: number,
+  sheet: WorkSheet
+): ASIProductImportData {
+  const sku = `${getSheetCellValue(ASI_SKU_COLUMN, rowNumber, sheet)}`;
+  const vendorName = `${getSheetCellValue(
+    ASI_PRIMARY_DATA_COLUMN,
+    rowNumber + ASI_VENDOR_NAME_ROW_OFFSET,
+    sheet
+  )}`;
+  const vendorSku = `${getSheetCellValue(
+    ASI_PRIMARY_DATA_COLUMN,
+    rowNumber + ASI_VENDOR_SKU_ROW_OFFSET,
+    sheet
+  )}`;
+  const description = `${getSheetCellValue(
+    ASI_PRIMARY_DATA_COLUMN,
+    rowNumber + ASI_DESCRIPTION_ROW_OFFSET,
+    sheet
+  )}`;
+  const priceBreaks: { quantity: string; price: string }[] = [];
+
+  for (
+    let i = ASI_PRICING_TABLE_START_COLUMN;
+    i < ASI_PRICING_TABLE_END_COLUMN + 1;
+    i++
+  ) {
+    const quantity = getSheetCellValue(
+      i,
+      rowNumber + ASI_PRICING_TABLE_ROW_OFFSET,
+      sheet
+    );
+    const price = getSheetCellValue(
+      i,
+      rowNumber + ASI_PRICING_TABLE_ROW_OFFSET + 1,
+      sheet
+    );
+
+    if (!quantity || !price) break;
+
+    if (quantity && price) priceBreaks.push({ quantity, price });
+  }
+
+  return { sku, vendorName, vendorSku, description, priceBreaks };
+}
