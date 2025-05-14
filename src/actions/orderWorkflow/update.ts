1614ae05eda067367eced8626c8191ff7cff5a47
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-05-14 09:33:10 -0500

    Allow editing and adding order metadata

--- src/actions/orderWorkflow/update.ts
+++ src/actions/orderWorkflow/update.ts
@@ -1,7 +1,11 @@
 "use server";
 
 import { getWorkflowWithIncludes } from "@/db/access/orderApproval";
-import { OrderUpdateData, updateOrder } from "@/fetch/woocommerce";
+import {
+  addMetaDataToOrder,
+  OrderUpdateData,
+  updateOrder,
+} from "@/fetch/woocommerce";
 import { decryptWebstoreData } from "@/order-approval/encryption";
 import { handleOrderUpdated } from "@/order-approval/main";
 import { UnwrapPromise } from "@/types/schema/misc";
@@ -172,6 +176,7 @@ export async function setUserEmail(id: number, email: string) {
 export async function updateOrderAction(
   storeUrl: string,
   updateData: OrderUpdateData,
+  metaDataToAdd: { key: string; value: string }[],
   userEmail: string //the email of the user initiating the update action
 ) {
   const webstore = await prisma.webstore.findUnique({
@@ -182,6 +187,21 @@ export async function updateOrderAction(
   if (!webstore) throw new Error(`Webstore with url ${storeUrl} not found.`);
 
   const { key, secret } = decryptWebstoreData(webstore);
+
+  if (metaDataToAdd.length > 0) {
+    const addMetaDataResponse = await addMetaDataToOrder(
+      updateData.id,
+      storeUrl,
+      key,
+      secret,
+      metaDataToAdd
+    );
+    if (!addMetaDataResponse.ok)
+      throw new Error(
+        `API response code ${addMetaDataResponse.status} when trying to add metadata to order ${updateData.id} for store ${storeUrl}`
+      );
+  }
+
   const updateResponse = await updateOrder(storeUrl, key, secret, updateData);
   if (!updateResponse.ok)
     throw new Error(
--- src/app/order-approval/OrderEditForm/CheckoutFields.tsx
+++ src/app/order-approval/OrderEditForm/CheckoutFields.tsx
@@ -1,15 +1,81 @@
-import { getFieldValue } from "@/order-approval/checkoutFields";
 import styles from "@/styles/orderApproval/orderEditForm.module.css";
 import { WooCommerceOrder } from "@/types/schema/woocommerce";
 import { WebstoreCheckoutField } from "@prisma/client";
 import { Updater } from "use-immer";
+import { MetaData } from "./OrderEditForm";
 
 type Props = {
   order: WooCommerceOrder;
   setOrder: Updater<WooCommerceOrder | null>;
   fields: Omit<WebstoreCheckoutField, "webstoreId">[];
+  metaDataToAdd: MetaData[];
+  setMetaDataToAdd: (val: MetaData[]) => void;
 };
-export function CheckoutFields({ order, setOrder, fields }: Props) {
+export function CheckoutFields({
+  order,
+  setOrder,
+  fields,
+  metaDataToAdd,
+  setMetaDataToAdd,
+}: Props) {
+  function onChangeField(key: string, value: string) {
+    //handle comments field differently since WooCommerce doesn't put it in the metadata
+    if (key === "order_comments") {
+      setOrder((draft) => {
+        if (draft) draft.customerNote = value;
+      });
+      return;
+    }
+
+    //if a metadata with the given key already exists, edit it directly and then exit
+    const metaDataExists = order.metaData.find((meta) => meta.key === key);
+    if (metaDataExists) {
+      setOrder((draft) => {
+        if (!draft) return;
+
+        const metaDataMatch = draft.metaData.find((meta) => meta.key === key);
+        if (!metaDataMatch) return;
+        metaDataMatch.value = value;
+      });
+      return;
+    }
+
+    //if we get here, a metadata with the given key does NOT exist in the data received from WooCommerce
+    //WC requires us to add it in a separate POST request, so keep track of what will be sent in "metaDataToAdd"
+    const additionalMetaDataExists = !!metaDataToAdd.find(
+      (meta) => meta.key === key
+    );
+    if (additionalMetaDataExists) {
+      if (!value) {
+        //if the user erased the value, we want to forget about this particular additional metadata
+        setMetaDataToAdd(metaDataToAdd.filter((meta) => meta.key !== key));
+      } else {
+        //but if there is a value, update the existing additional metadata
+        const newAdditionalMetaData = metaDataToAdd.map((meta) =>
+          meta.key === key ? { key, value } : meta
+        );
+        setMetaDataToAdd(newAdditionalMetaData);
+      }
+    } else {
+      setMetaDataToAdd([...metaDataToAdd, { key, value }]);
+    }
+  }
+
+  function getFieldValue(fieldName: string) {
+    if (["order_comments", "customer_note"].includes(fieldName))
+      return order.customerNote;
+
+    const matchingMetaData = order.metaData.find(
+      (meta) => meta.key === fieldName
+    );
+    if (matchingMetaData) return matchingMetaData.value;
+
+    const matchingAdditionalMetaData = metaDataToAdd.find(
+      (meta) => meta.key === fieldName
+    );
+    return matchingAdditionalMetaData?.value || "";
+  }
+
   return (
     <div>
       <h3>Additional Information</h3>
@@ -23,9 +89,8 @@ export function CheckoutFields({ order, setOrder, fields }: Props) {
                 type="text"
                 name={field.name}
                 id={field.name}
-                value={getFieldValue(field.name, order)}
-                onChange={() => {}}
-                disabled
+                value={getFieldValue(field.name)}
+                onChange={(e) => onChangeField(field.name, e.target.value)}
               />
             )}
 
@@ -35,15 +100,8 @@ export function CheckoutFields({ order, setOrder, fields }: Props) {
                 id={field.name}
                 cols={40}
                 rows={5}
-                value={getFieldValue(field.name, order)}
-                onChange={(e) => {
-                  if (field.name === "order_comments") {
-                    setOrder((draft) => {
-                      if (draft) draft.customerNote = e.target.value;
-                    });
-                  }
-                }}
-                disabled={field.name !== "order_comments"}
+                value={getFieldValue(field.name)}
+                onChange={(e) => onChangeField(field.name, e.target.value)}
               />
             )}
 
@@ -51,8 +109,8 @@ export function CheckoutFields({ order, setOrder, fields }: Props) {
               <select
                 name={field.name}
                 id={field.name}
-                value={getFieldValue(field.name, order)}
-                disabled
+                value={getFieldValue(field.name)}
+                onChange={(e) => onChangeField(field.name, e.target.value)}
               >
                 {(field.options || "").split(/\s*\|\s*/g).map((option) => (
                   <option key={option} value={option}>
