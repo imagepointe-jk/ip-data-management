1cbe58682671446d1459c1315f5877d3fff57b9b
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-07-01 09:12:30 -0500

    Add pagination to backend customizer products

--- src/app/admin/customizer/page.tsx
+++ src/app/admin/customizer/page.tsx
@@ -1,16 +1,40 @@
-import { getProductSettings } from "@/db/access/customizer";
-import ResultsTable from "./ResultsTable";
 import { populateProductData } from "@/app/customizer/handleData";
 import { CreateProduct } from "./CreateProduct";
+import { ResultsExplorer } from "./ResultsExplorer";
+import { prisma } from "@/prisma";
+import { NextSearchParams } from "@/types/schema/misc";
 
-export default async function Customizer() {
-  const productSettings = await getProductSettings();
+type Props = {
+  searchParams: NextSearchParams;
+};
+export default async function Customizer({ searchParams }: Props) {
+  const search = await searchParams;
+  const page = isNaN(+`${search.page}`) ? 1 : +`${search.page}`;
+  const pageSize = 5;
+  const [productSettings, allProductSettings] = await prisma.$transaction([
+    prisma.customProductSettings.findMany({
+      include: {
+        variations: {
+          include: {
+            views: true,
+          },
+        },
+      },
+      take: pageSize,
+      skip: pageSize * (page - 1),
+    }),
+    prisma.customProductSettings.findMany(),
+  ]);
   const populatedProducts = await populateProductData(productSettings);
 
   return (
     <>
       <h1>Customizable Products</h1>
-      <ResultsTable productListings={populatedProducts} />
+      <ResultsExplorer
+        productListings={populatedProducts}
+        pageSize={pageSize}
+        totalRecords={allProductSettings.length}
+      />
       <CreateProduct />
     </>
   );
