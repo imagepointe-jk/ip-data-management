d4f06f655e9c1687e26f54e72fd176e86b77dd64
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2025-10-01 11:51:15 -0500

    Add displayed handling fee on order approval page

--- src/app/order-approval/OrderEditForm/OrderTotals.tsx
+++ src/app/order-approval/OrderEditForm/OrderTotals.tsx
@@ -5,15 +5,22 @@ type Props = {
   order: WooCommerceOrder;
 };
 export function OrderTotals({ order }: Props) {
-  const totalNoShipping = +order.subtotal + +order.totalTax;
+  const handlingFee = order.feeLines.find(
+    (line) => line.name.toLocaleLowerCase() === "handling fee"
+  );
+  const handlingFeeAmount = handlingFee?.total ? +handlingFee.total : 0;
+  const totalToDisplay = +order.subtotal + +order.totalTax + handlingFeeAmount;
 
   return (
     <div className={styles["main"]}>
       <div>Item Total: ${order.subtotal}</div>
       <div>Sales Tax: ${order.totalTax}</div>
       <div>
+        Handling Fee: {handlingFee ? `$${handlingFee.total}` : "Billed"}
+      </div>
+      <div>
         <span className={styles["emph"]}>Total w/o Shipping:</span> $
-        {totalNoShipping.toFixed(2)}
+        {totalToDisplay.toFixed(2)}
       </div>
     </div>
   );
