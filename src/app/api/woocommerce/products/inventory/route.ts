82ba790ac4f5b9edb55873614f1944472a69c12f
imagepointe-jk <148394472+imagepointe-jk@users.noreply.github.com>
2026-07-09 10:21:19 -0500

    Test webhook

--- src/app/api/woocommerce/products/inventory/route.ts
+++ src/app/api/woocommerce/products/inventory/route.ts
@@ -3,8 +3,20 @@ import { NextRequest } from "next/server";
 import { inspect } from "util";
 
 export async function POST(request: NextRequest) {
-  const json = await request.json();
-  console.log(inspect(json, true, null));
+  try {
+    const json = await request.json();
+    console.log(inspect(json, true, null));
+  } catch (error) {
+    console.log("Unable to parse json");
+  }
+
+  try {
+    const text = await request.text();
+    console.log(text);
+  } catch (error) {
+    console.log("unable to parse text");
+  }
+
   //Always send a 200 response back to WooCommerce; the webhook seems to break otherwise
   return Response.json({}, easyCorsInit);
 }
